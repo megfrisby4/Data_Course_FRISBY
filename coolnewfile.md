46da89c1673143704d4bf245a4a886e9495e73a9
Megan Frisby <frisbymegan4@gmail.com>
2023-01-20 18:28:07 -0700

    commiting 1-20

--- coolnewfile.md
+++
@@ -1,1 +0,0 @@
-This README file contains information about my uploaded assignments
--- week 2.R
+++
@@ -1,3 +0,0 @@
-# class notes January 17, 2023
-# hashtg means everthing to the right on that line 
-#is for humans, wont be read as R code 
