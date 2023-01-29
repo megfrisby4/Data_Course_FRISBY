2fa4b8a59dd580090c170d781f3380da5b1fdcb3
Megan Frisby <frisbymegan4@gmail.com>
2023-01-29 09:41:07 -0700

    adding notes

--- Notes/teams jan 25 actual solution.R
+++ Notes/teams jan 25 actual solution.R
@@ -1,3 +1,4 @@
+
 rectangles <- read.csv("Data/rectangles.csv")
 rectangles
 class(rectangles)
@@ -5,23 +6,13 @@ rectangles$length
 length(rectangles$length)
 class(rectangles$length)
 as.numeric(rectangles$length
-           )
+)
 length <- as.numeric(rectangles$length
 )
 width <- as.numeric(rectangles$width)
-na.exclude(width,)
-area <- rectangles$area <- length*width
-length(rectangles$area)
-area[62]
-
-rectangles
-
-#or instead of excluding the data just write it in
-rectangles$width[63] = 231
 width
-width[63]
 rectangles$area <- length*width
-area <- rectangles$area
-
-
 rectangles
+write.csv(rectangles, "./Data/rectangles_clean.csv")
+list.files(path= "./Data/", pattern="rectangles")
+
