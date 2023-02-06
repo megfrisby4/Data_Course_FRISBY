cce6cf6c7bb170867e19d7b93e9f08a996927c06
Megan Frisby <frisbymegan4@gmail.com>
2023-02-05 19:06:48 -0700

    adding assignment 4

---
+++ Assignments/Assignment_4/final_project_idea_plot.R
@@ -0,0 +1,11 @@
+install.packages("tidyverse")
+library(tidyverse)
+df <- read.csv("./Assignments/Assignment_4/final_project_idea_data .csv")
+class(df)
+  df %>% ggplot(aes(x=country)) + geom_bar(aes(fill= Year )) +
+    theme(axis.text.x = element_text(angle = 70, hjust=1),
+          axis.title.x = element_text(vjust = 10)) + labs(y="Number of Outbreaks") +
+    scale_fill_manual(values =c("red", "orange", "yellow", "green", "blue", "navy", "violet","purple", "pink", "magenta", "#320354",
+                                     "#f0056b", "#f03c05"))
+  
+
