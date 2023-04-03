c37826fb3201eb45485cc7961a8fd4b67a4a81c9
Megan Frisby <frisbymegan4@gmail.com>
2023-04-03 10:27:13 -0600

    fixing

---
+++ Final_Help/building_a_phylo_tree.R
@@ -0,0 +1,18 @@
+DECIPHER:: AlignSeqs()
+
+# once alignment get 
+
+Phangorn:: online tutorial 
+
+
+ggtree 
+ape:: 
+  
+# fasta files 
+  
+ShortRead:: readFasta()
+#dont treat it like text... DNAStringSet
+
+my_function <- function(x,y=10){
+  z <- 
+}
